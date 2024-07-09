208d2fa5b8f5d613d9a4dddf897f43537e3d723e
duckmaniac <142285787+trananhduck@users.noreply.github.com>
2024-07-09 10:08:28 +0700

    Update Test.java
    Signed-off-by: duckmaniac <142285787+trananhduck@users.noreply.github.com>

--- Search Words/src/test/Test.java
+++ Search Words/src/test/Test.java
@@ -1,9 +1,15 @@
 package test;
 
+import javax.swing.UIManager;
 import view.TimKiemView;
 
 public class Test {
     public static void main(String[] args) {
-        new TimKiemView();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            new TimKiemView();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
